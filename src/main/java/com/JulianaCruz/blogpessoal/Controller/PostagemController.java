23abfc80485b05f7020656d05eff033308a001a6
juliana035 <juliana.ferreira_cruz@yahoo.com.br>
2022-09-30 17:43:34 -0300

    altercaocontroller

--- src/main/java/com/JulianaCruz/blogpessoal/Controller/PostagemController.java
+++ src/main/java/com/JulianaCruz/blogpessoal/Controller/PostagemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
-
+    @GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 	return ResponseEntity.ok(postagemRepository.findAll());
 	}
