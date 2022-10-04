8df388f35569bda3941e9932b3f26764358015d6
juliana035 <juliana.ferreira_cruz@yahoo.com.br>
2022-10-03 21:41:04 -0300

    put,delete,post

--- src/main/java/com/JulianaCruz/blogpessoal/Repository/PostagemRepository.java
+++ src/main/java/com/JulianaCruz/blogpessoal/Repository/PostagemRepository.java
@@ -3,7 +3,10 @@ package com.JulianaCruz.blogpessoal.Repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.JulianaCruz.blogpessoal.model.Postagem;
@@ -11,6 +14,5 @@ import com.JulianaCruz.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem,Long>{
 
-	
-	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 }
